4081c6f1a835ca64ceeb310001caef75cd2616ce
premsai7744 <premsai7744@gmail.com>
2026-06-21 21:03:21 +0530

    Initial commit.

---
+++ src/main/java/com/premit/SpringBootRestApiUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.premit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiUserRegistrationApplication.class, args);
+	}
+
+}
