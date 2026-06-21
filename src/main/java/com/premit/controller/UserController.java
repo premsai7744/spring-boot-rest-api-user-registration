a466b78b884bac8a125e5da458af24686aad0280
premsai7744 <premsai7744@gmail.com>
2026-06-21 23:43:59 +0530

    Initialize User Controller layer.

--- src/main/java/com/premit/controller/UserController.java
+++ src/main/java/com/premit/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.premit.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.premit.dto.UserDto;
+import com.premit.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@PostMapping(path="/register",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public String userRegistration(@RequestBody UserDto userDto) {
+		System.out.println("UserController.userRegistration()::DataReceived::userDto:"+userDto);
+	 	String result = userService.userRegistration(userDto);
+		return result;
+	}
+	
 }
